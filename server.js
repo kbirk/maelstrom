f7b6dec8bc0d93afeedbe42c431d0242a485a11d
kbirk <birk.kevin@gmail.com>
2015-06-14 18:28:20 -0400

    Added server.js file.

---
+++ server.js
@@ -0,0 +1,15 @@
+(function() {
+
+    "use strict";
+
+    var express = require( 'express' ),
+        bodyParser = require( 'body-parser' ),
+        app = express();
+    app.use( bodyParser.json() ); // support JSON-encoded bodies
+    app.use( bodyParser.urlencoded({ extended: false }) ); // support URL-encoded bodies
+    app.use( express.static( __dirname + "/build/" ) );
+    app.listen( 8080, '127.0.0.1', function() {
+        console.log( 'Listening on port %d', 8080 );
+    });
+
+}());
