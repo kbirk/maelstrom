9e8543a2a86a9e528732537db227a97954cff434
kbirk <birk.kevin@gmail.com>
2015-08-17 00:49:46 -0400

    Added expressjs compression middleware.

--- gulpfile.js
+++ gulpfile.js
@@ -113,9 +113,11 @@
     gulp.task('serve', ['build'], function() {
         var express = require( 'express' ),
             bodyParser = require( 'body-parser' ),
+            compression = require( 'compression' ),
             app = express();
         app.use( bodyParser.json() ); // support JSON-encoded bodies
         app.use( bodyParser.urlencoded({ extended: false }) ); // support URL-encoded bodies
+        app.use( compression() );
         app.use( express.static( __dirname + "/build/" ) );
         app.listen( 8080, function() {
             console.log( 'Listening on port %d', 8080 );
--- server.js
+++ server.js
@@ -4,9 +4,11 @@
 
     var express = require( 'express' ),
         bodyParser = require( 'body-parser' ),
+        compression = require( 'compression' ),
         app = express();
     app.use( bodyParser.json() ); // support JSON-encoded bodies
     app.use( bodyParser.urlencoded({ extended: false }) ); // support URL-encoded bodies
+    app.use( compression() );
     app.use( express.static( __dirname + "/build/" ) );
     app.listen( 8080, '127.0.0.1', function() {
         console.log( 'Listening on port %d', 8080 );
